74f2105d76e91365415a2b38fdbcac50530821e2
8x8chicken <understateddragon@gmail.com>
2025-02-01 16:11:36 -0500

    update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738444022|146757301';
+const CACHE_VERSION = '1738444264|388827313';
 /** @type {string} */
-const CACHE_PREFIX = 'Beckett-sw-cache-';
+const CACHE_PREFIX = 'Courinotes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';

2c58e8e3773418ea277e966cdadaa26537fc2fce
8x8chicken <understateddragon@gmail.com>
2025-02-02 10:54:50 -0500

    added copy support for outputs

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738508200|1582647845';
+const CACHE_VERSION = '1738511642|5024560326';
 /** @type {string} */
 const CACHE_PREFIX = 'Courinotes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

129ede90d28124aa9a72cb499eae9b6674a01424
8x8chicken <understateddragon@gmail.com>
2025-02-02 22:22:05 -0500

    updated to latest notes list

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738511642|5024560326';
+const CACHE_VERSION = '1738552856|2032037677';
 /** @type {string} */
 const CACHE_PREFIX = 'Courinotes-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
